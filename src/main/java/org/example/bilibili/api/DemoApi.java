1c75c0ad29df24c29705533d2c4fc8ef9144dbd9
BillyWu423 <zihengwubilly@outlook.com>
2025-07-13 14:32:01 -0400

    Initial commit

---
+++ src/main/java/org/example/bilibili/api/DemoApi.java
@@ -0,0 +1,30 @@
+package org.example.bilibili.api;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+public class DemoApi {
+
+    @GetMapping("/demos")
+    public Long msget(@RequestParam Long id) {
+        return id;
+    }
+
+    @PostMapping("/demos")
+    public Map<String, Object> mspost(@RequestBody Map<String, Object> params) {
+        return params;
+    }
+
+    @GetMapping("/timeout")
+    public String timeout(@RequestParam Long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "circuit breaker testing";
+    }
+
+}
